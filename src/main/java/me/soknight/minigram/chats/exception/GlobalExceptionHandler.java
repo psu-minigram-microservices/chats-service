9267167dde87676103aaa2921f28dad12e5881ca
SoKnight <mcliyanage2018@gmail.com>
2026-03-10 00:04:55 +0500

    chore: use jspecify nullability annotations

--- src/main/java/me/soknight/minigram/chats/exception/GlobalExceptionHandler.java
+++ src/main/java/me/soknight/minigram/chats/exception/GlobalExceptionHandler.java
@@ -2,9 +2,9 @@ package me.soknight.minigram.chats.exception;
 
 import jakarta.validation.ConstraintViolationException;
 import me.soknight.minigram.chats.model.ErrorModel;
+import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,30 +13,32 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(GenericErrorException.class)
-    public ResponseEntity<ErrorModel> handleGenericError(GenericErrorException ex) {
+    public @NonNull ResponseEntity<ErrorModel> handleGenericError(@NonNull GenericErrorException ex) {
         return ResponseEntity.status(ex.getStatusCode()).body(ex.constructModel());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorModel> handleValidationError(MethodArgumentNotValidException ex) {
-        FieldError firstError = ex.getBindingResult().getFieldErrors().stream().findFirst().orElse(null);
-        GenericErrorException error = firstError == null
+    public @NonNull ResponseEntity<ErrorModel> handleValidationError(@NonNull MethodArgumentNotValidException ex) {
+        var firstError = ex.getBindingResult().getFieldErrors().stream().findFirst().orElse(null);
+        var error = firstError == null
                 ? new GenericErrorException(HttpStatus.BAD_REQUEST, "incorrect_field_value", "Request body validation failed")
                 : GenericErrorException.fromFieldError(firstError);
+
         return ResponseEntity.status(error.getStatusCode()).body(error.constructModel());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ErrorModel> handleConstraintViolation(ConstraintViolationException ex) {
-        GenericErrorException error = ex.getConstraintViolations().stream().findFirst()
+    public @NonNull ResponseEntity<ErrorModel> handleConstraintViolation(@NonNull ConstraintViolationException ex) {
+        var error = ex.getConstraintViolations().stream().findFirst()
                 .map(GenericErrorException::fromConstraintViolation)
                 .orElseGet(() -> new GenericErrorException(HttpStatus.BAD_REQUEST, "incorrect_field_value", "Request validation failed"));
+
         return ResponseEntity.status(error.getStatusCode()).body(error.constructModel());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorModel> handleUnexpectedError(Exception ex) {
-        GenericErrorException error = new GenericErrorException(ex);
+    public @NonNull ResponseEntity<ErrorModel> handleUnexpectedError(@NonNull Exception ex) {
+        var error = new GenericErrorException(ex);
         return ResponseEntity.status(error.getStatusCode()).body(error.constructModel());
     }
 
