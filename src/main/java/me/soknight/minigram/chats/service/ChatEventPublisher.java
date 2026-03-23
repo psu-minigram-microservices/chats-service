536e9c0f5654ae82c53c96d7107173915d1ad1f3
SoKnight <mcliyanage2018@gmail.com>
2026-03-24 00:42:50 +0500

    fix: ws event publishing

--- src/main/java/me/soknight/minigram/chats/service/ChatEventPublisher.java
+++ src/main/java/me/soknight/minigram/chats/service/ChatEventPublisher.java
@@ -6,8 +6,10 @@ import me.soknight.minigram.chats.repository.ChatMemberRepository;
 import org.jspecify.annotations.NonNull;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import java.lang.Iterable;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -25,6 +27,21 @@ public class ChatEventPublisher {
     }
 
     public void publishToUsers(@NonNull Iterable<UUID> userIds, @NonNull ChatEvent event) {
+        var recipients = List.copyOf((java.util.Collection<UUID>) userIds);
+
+        if (TransactionSynchronizationManager.isSynchronizationActive()) {
+            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+                @Override
+                public void afterCommit() {
+                    sendToUsers(recipients, event);
+                }
+            });
+        } else {
+            sendToUsers(recipients, event);
+        }
+    }
+
+    private void sendToUsers(@NonNull Iterable<UUID> userIds, @NonNull ChatEvent event) {
         for (UUID userId : userIds) {
             messagingTemplate.convertAndSendToUser(
                     userId.toString(),
