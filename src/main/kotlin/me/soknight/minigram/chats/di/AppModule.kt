aeaac5798e48759a70b05b4dc48d8c4232bdc1c2
SoKnight <mcliyanage2018@gmail.com>
2026-05-11 00:19:16 +0500

    fix: consolidate all providers into AppModule, remove ConfigProviders.kt
    Top-level @Single functions generate separate Koin modules not included
    in AppModule's generated .module property. Moving provideHttpClient()
    into AppModule ensures a single modules(AppModule(this).module) call
    registers all bindings.

--- src/main/kotlin/me/soknight/minigram/chats/di/AppModule.kt
+++ src/main/kotlin/me/soknight/minigram/chats/di/AppModule.kt
@@ -1,7 +1,12 @@
 package me.soknight.minigram.chats.di
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import me.soknight.minigram.chats.config.*
+import me.soknight.minigram.chats.plugin.appJson
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
@@ -13,4 +18,10 @@ class AppModule(private val application: Application) {
     @Single fun provideJwtConfig(config: AppConfig): JwtConfig = config.jwt
     @Single fun provideDatabaseConfig(config: AppConfig): DatabaseConfig = config.database
     @Single fun provideServicesConfig(config: AppConfig): ServicesConfig = config.services
+
+    @Single
+    fun provideHttpClient(): HttpClient = HttpClient(CIO) {
+        install(ContentNegotiation) { json(appJson) }
+        expectSuccess = true
+    }
 }
