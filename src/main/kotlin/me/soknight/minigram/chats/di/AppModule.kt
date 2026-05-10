9829d32f2e0fdc53e9b08f6ffe8b8ebefca78185
SoKnight <mcliyanage2018@gmail.com>
2026-05-11 00:15:03 +0500

    feat: add Koin DI module structure and config providers
    AppModule takes Application as constructor param so config providers
    don't depend on a Koin-provided Application (which is only available
    at runtime via koin-ktor), satisfying the compiler plugin's
    compile-time dependency validation.

--- src/main/kotlin/me/soknight/minigram/chats/di/AppModule.kt
+++ src/main/kotlin/me/soknight/minigram/chats/di/AppModule.kt
@@ -1,8 +1,16 @@
 package me.soknight.minigram.chats.di
 
+import io.ktor.server.application.*
+import me.soknight.minigram.chats.config.*
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 
 @Module
 @ComponentScan("me.soknight.minigram.chats")
-class AppModule
+class AppModule(private val application: Application) {
+    @Single fun provideAppConfig(): AppConfig = application.loadConfig()
+    @Single fun provideJwtConfig(config: AppConfig): JwtConfig = config.jwt
+    @Single fun provideDatabaseConfig(config: AppConfig): DatabaseConfig = config.database
+    @Single fun provideServicesConfig(config: AppConfig): ServicesConfig = config.services
+}
