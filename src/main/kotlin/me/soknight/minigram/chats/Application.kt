2ca1480f319ebc0655c01a63a8bfc417ab75f8b1
SoKnight <mcliyanage2018@gmail.com>
2026-05-11 00:25:13 +0500

    refactor: replace manual DI wiring with Koin in Application.kt

--- src/main/kotlin/me/soknight/minigram/chats/Application.kt
+++ src/main/kotlin/me/soknight/minigram/chats/Application.kt
@@ -1,58 +1,31 @@
 package me.soknight.minigram.chats
 
-import io.ktor.client.*
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
-import me.soknight.minigram.chats.auth.JwtTokenProvider
-import me.soknight.minigram.chats.client.ProfileClient
-import me.soknight.minigram.chats.config.loadConfig
+import me.soknight.minigram.chats.di.AppModule
 import me.soknight.minigram.chats.plugin.*
-import me.soknight.minigram.chats.repository.ChatMemberRepository
-import me.soknight.minigram.chats.repository.ChatMessageRepository
-import me.soknight.minigram.chats.repository.ChatRepository
-import me.soknight.minigram.chats.service.*
-import me.soknight.minigram.chats.websocket.WebSocketConnectionManager
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import org.koin.ktor.ext.get
+import org.koin.ktor.plugin.Koin
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
 }
 
-fun Application.module(clientFactory: ((String) -> ProfileClient)? = null) {
-    val config = loadConfig()
-
-    configureDatabase(config.database)
-    configureSerialization()
-    configureSecurity(config.jwt)
-    configureStatusPages()
-
-    val jwtProvider       = JwtTokenProvider(config.jwt)
-    val connectionManager = WebSocketConnectionManager()
-
-    val chatRepo    = ChatRepository()
-    val memberRepo  = ChatMemberRepository()
-    val messageRepo = ChatMessageRepository()
-
-    val dtoMapper      = ChatDtoMapper(chatRepo, memberRepo)
-    val eventPublisher = ChatEventPublisher(connectionManager, memberRepo)
-
-    val chatService    = ChatService(chatRepo, memberRepo, dtoMapper, eventPublisher)
-    val memberService  = ChatMemberService(chatRepo, memberRepo, dtoMapper, eventPublisher)
-    val messageService = ChatMessageService(chatRepo, memberRepo, messageRepo, dtoMapper, eventPublisher)
-
-    val resolvedClientFactory: (String) -> ProfileClient = clientFactory ?: run {
-        val httpClient = HttpClient(CIO) {
-            install(ContentNegotiation) { json(appJson) }
-            expectSuccess = true
-        }
-        val factory: (String) -> ProfileClient = { token ->
-            ProfileClient(httpClient, config.services.profileUrl, token)
-        }
-        factory
+fun Application.module(vararg extraModules: Module = emptyArray()) {
+    val app = this
+    install(Koin) {
+        modules(
+            module { single { AppModule(app) } },
+            *extraModules
+        )
     }
 
-    configureRouting(chatService, memberService, messageService, resolvedClientFactory)
-    configureWebSockets(jwtProvider, connectionManager)
+    configureDatabase(get())
+    configureSerialization()
+    configureSecurity(get())
+    configureStatusPages()
+    configureRouting(get(), get(), get(), get())
+    configureWebSockets(get(), get())
 }
