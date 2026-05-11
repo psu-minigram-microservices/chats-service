6a521931f37e83de819b2d8bcbbf2b7fc6777f9a
SoKnight <mcliyanage2018@gmail.com>
2026-05-12 00:09:15 +0500

    fix: use lowercase SerialName for all serializable enums

--- src/main/kotlin/me/soknight/minigram/chats/model/Enums.kt
+++ src/main/kotlin/me/soknight/minigram/chats/model/Enums.kt
@@ -1,14 +1,26 @@
 package me.soknight.minigram.chats.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class ChatType { SAVED, DIRECT, GROUP }
+enum class ChatType {
+    @SerialName("saved") SAVED,
+    @SerialName("direct") DIRECT,
+    @SerialName("group") GROUP
+}
 
 @Serializable
-enum class ChatMemberRole { OWNER, MEMBER }
+enum class ChatMemberRole {
+    @SerialName("owner") OWNER,
+    @SerialName("member") MEMBER
+}
 
 @Serializable
-enum class RelationStatus { NONE, FRIEND, BLOCKED }
+enum class RelationStatus {
+    @SerialName("none") NONE,
+    @SerialName("friend") FRIEND,
+    @SerialName("blocked") BLOCKED
+}
 
 enum class RelationType { INCOMING, OUTGOING }
